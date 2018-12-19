bec1adfe56b71d37f618245375cc6e997996ce14
Jonathan Ackerman <jla617@gmail.com>
2018-12-18 22:24:23 -0800

    Update temp.js

--- temp.js
+++ temp.js
@@ -22,7 +22,7 @@ function highlightItem(input)
 
 	for (itemIter = 0; itemIter < gItemList.length; ++itemIter)
 	{
-		if(compare(input, gItemList[itemIter]) > 0)
+		if(JaroWinkler_Similarity(input, gItemList[itemIter]) > 0)
 		{
 			foundArray.push(gItemList[itemIter]);
 		}
@@ -47,24 +47,56 @@ function HighlightFoundItems()
 	}
 }
 
-function compare(lhs, rhs)
+function JaroWinkler_Similarity(input, prototype)
 {
-	left = lhs.toLowerCase();
-	right = rhs.toLowerCase();
-	
-	var len = 0;
-	if (left.length < right.length)
-		len = left.length;
-	else
-		len = right.length;
-
-    var count = 0;
-	for (letterIter = 0; letterIter < len; ++letterIter)
+    if (input.length == 0)
+        return 0;
+	left = input.toLowerCase();
+	right = prototype.toLowerCase();
+    
+    var matchDist = Math.floor(Math.max(left.length, right.length) / 2) - 1;
+    var numMatches = 0;
+    var numFlips = 0;
+    (leftMatches = []).length = left.length;
+    leftMatches.fill(false);
+    (rightMatches = []).length = right.length;
+    rightMatches.fill(false);
+    
+    //there might be issues with duplicates in either string
+	for (leftIter = 0; leftIter < left.length; ++leftIter)
 	{
-		if (left[letterIter] != right[letterIter])
+        for (rightIter = 0; rightIter < right.length; ++rightIter)
         {
-            return letterIter;
+            if (left[leftIter] == right[rightIter] && Math.abs(rightIter - leftIter) <= matchDist)
+            {
+                ++numMatches;
+                leftMatches[leftIter] = true;
+                rightMatches[rightIter] = true;
+            }
         }
 	}
-    return len;
+    
+    var j = 0;
+    var t = 0;
+    for (var i = 0; i < left.length; ++i)
+    {
+        if (leftMatches[i])
+        {
+            while (!rightMatches[j])
+                ++j;
+            if (left[i] != right[j])
+                t += 0.5;
+            ++j;
+        }
+    }
+    
+    var jaro = 0;
+    if (numMatches > 0)
+    {
+        jaro = (1/3) * ((numMatches / left.length) + (numMatches / right.length) + ((numMatches - numFlips) / numMatches));
+    }
+    
+    var prefixScale = 0.1; // 0.1 is the std
+    var prefixLen = 2; // prefix is 0..4
+    return jaro + (prefixLen * prefixScale * (1 - jaro));
 }
